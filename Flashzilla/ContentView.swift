afe7774ca7b49702a0a0fc09ca4115248c807c88
solankinitish <nitishsolanki888@gmail.com>
2022-05-09 05:44:57 +0530

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Nitish Solanki on 09/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
