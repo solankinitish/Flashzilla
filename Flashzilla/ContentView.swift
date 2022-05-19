7e42582315923a1983dfb062bd35150edc912cca
Nitish <nitishsolanki888@gmail.com>
2022-05-19 06:06:45 +0530

    Initial Commit

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
+    
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .rotationEffect(finalAmount + currentAmount)
+            .gesture(
+                RotationGesture()
+                    .onChanged { angle in
+                        currentAmount = angle
+                    }
+                    .onEnded { angle in
+                        finalAmount += currentAmount
+                        currentAmount =  .zero
+                    }
+            
+            )
+
     }
 }
 
